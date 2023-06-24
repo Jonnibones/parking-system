10ebaf1c32e10d443b29856cc37a9f38554c5bda
Jonnibones <js5800672@gmail.com>
2023-06-24 12:38:43 -0300

    Ajuste em master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,10 +1,29 @@
-<?php $base_url = config('app.url');?>
+<?php 
+    use Illuminate\Support\Facades\DB;
+
+    $base_url = config('app.url');
+
+    $numberSpacesBadge['Occupied'] = DB::table('parking_spaces')
+    ->where('status', '=', 'Ocupado')
+    ->count();
+    $numberSpacesBadge['Reserved'] = DB::table('parking_spaces')
+    ->where('status', '=', 'Reservado')
+    ->count();
+    $numberSpacesBadge['Liberated'] = DB::table('parking_spaces')
+    ->where('status', '=', 'Liberado')
+    ->count();
+
+    $badges = [
+        'numberSpacesBadge' => $numberSpacesBadge,
+    ];
+
+?>
 
 @include('layouts/header')
 
 @include('layouts/navbar', ['base_url' => $base_url])
 
-@include('layouts/sidebar')
+@include('layouts/sidebar', ['contents' => $badges])
 
 @include('pages/'.$contents['view'])
 
